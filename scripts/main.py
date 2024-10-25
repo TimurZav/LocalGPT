9b7da44cb1075ec1062cdfc51c94d378ddac24a4
timur <timurzavalov@gmail.com>
2024-10-25 23:27:06 +0300

    git init

--- scripts/main.py
+++ scripts/main.py
@@ -9,7 +9,7 @@ from server.health.health_router import health_router
 app = FastAPI()
 app.include_router(health_router)
 app.include_router(auth_router)
-gr.mount_gradio_app(app, LocalGPT().run(), path="/")
+gr.mount_gradio_app(app, LocalGPT().launch_ui(), path="/")
 
 
 if __name__ == "__main__":
