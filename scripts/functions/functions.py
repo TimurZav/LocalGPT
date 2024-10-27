411589b19ea612eeb55e80825221634ed0d32118
timur <timurzavalov@gmail.com>
2024-10-28 01:19:50 +0300

    add: added multimodal llm

--- scripts/functions/functions.py
+++ scripts/functions/functions.py
@@ -1,5 +1,5 @@
 import requests
-from functions.__init__ import DATABASE_URL
+from scripts.functions.__init__ import DATABASE_URL
 from sqlalchemy import create_engine, text
 
 # Функция для интеграции в LLM через tools
